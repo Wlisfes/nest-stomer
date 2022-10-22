ac55ed4286f4e0e6785d3824513fe4fde39d94fd
妖雨纯 <876451336@qq.com>
2022-10-22 23:18:01 +0800

    ✨ feat(system): 新增字典配置

---
+++ src/views/manager/system/Chacter.vue
@@ -0,0 +1,11 @@
+<script lang="tsx">
+import { defineComponent } from 'vue'
+import { AppContainer } from '@/components/global'
+
+export default defineComponent({
+    name: 'Chacter',
+    setup() {
+        return () => <AppContainer>Chacter</AppContainer>
+    }
+})
+</script>
