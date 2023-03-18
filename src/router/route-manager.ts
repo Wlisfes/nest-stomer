0333115af0fc6f8c3a57513340733e3b46c8d3bf
妖雨纯 <876451336@qq.com>
2023-03-19 01:57:22 +0800

    ✨ feat(Manager): 新增主控台

--- src/router/route-manager.ts
+++ src/router/route-manager.ts
@@ -1,19 +1,18 @@
 import { RouteRecordRaw } from 'vue-router'
-import Layout from '@/layout/manager/Layout.vue'
 
 export const routeManager: Array<RouteRecordRaw> = [
-    // {
-    //     path: '/manager',
-    //     name: 'Layout',
-    //     component: Layout,
-    //     redirect: '/manager/master',
-    //     children: [
-    //         {
-    //             path: '/manager/master',
-    //             name: 'MHome',
-    //             meta: { title: '主控台' },
-    //             component: () => import('@/views/manager/Home.vue')
-    //         }
-    //     ]
-    // }
+    {
+        path: '/manager',
+        name: 'Layout',
+        component: () => import('@/layout/manager/Layout.vue'),
+        redirect: '/manager/master',
+        children: [
+            {
+                path: '/manager/master',
+                name: 'Master',
+                meta: { title: '主控台' },
+                component: () => import('@/views/manager/basic/Master.vue')
+            }
+        ]
+    }
 ]
