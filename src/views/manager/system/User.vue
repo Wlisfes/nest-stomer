9ff5779180194fb8c709a6a341e7a7dff8596311
Wlisfes <876451336@qq.com>
2023-03-22 22:05:19 +0800

    ✨ feat(basic): 新增基础表格组件

--- src/views/manager/system/User.vue
+++ src/views/manager/system/User.vue
@@ -1,10 +1,52 @@
 <script lang="tsx">
 import { defineComponent } from 'vue'
+import { DataTableBaseColumn } from 'naive-ui'
+import { useProvider } from '@/hooks/hook-provider'
+import { useColumn } from '@/hooks/hook-column'
+import { useSource } from '@/hooks/hook-source'
+import { httpColumn, IRouter } from '@/api/fetch-router'
 
 export default defineComponent({
     name: 'User',
     setup() {
-        return () => <u-container>User</u-container>
+        const { vars } = useProvider()
+        const { divineColumn, divineRxicon, divineCmule, divineCommand } = useColumn()
+        const { state, setState, fetchUpdate } = useSource<IRouter, Object>({
+            dataColumn: [
+                { title: '名称', key: 'title', minWidth: 200 },
+                { title: '图标', key: 'icon', width: 120 },
+                { title: '类型', key: 'type', width: 120 },
+                { title: '节点路由', key: 'path', minWidth: 200, ellipsis: { tooltip: true } },
+                { title: '更新时间', key: 'updateTime', align: 'center', width: 180 },
+                { title: '操作', key: 'command', fixed: 'right', align: 'center', width: 160 }
+            ],
+            immediate: true,
+            init: e => httpColumn()
+        })
+
+        const render = (value: unknown, row: IRouter, base: DataTableBaseColumn) => {
+            const __COLUME__ = {
+                icon: () => divineColumn(value, divineRxicon(row.icon, { depth: 1 }, { cursor: 'pointer' })),
+                type: () => {
+                    if (value === 1) {
+                        return divineCmule('目录', { type: 'info', bordered: false }, { class: 'naive-customize' })
+                    } else if (value === 2) {
+                        return divineCmule('目录', { type: 'success', bordered: false }, { class: 'naive-customize' })
+                    }
+                },
+                command: () => {
+                    return divineCommand(row, { native: ['edit'] })
+                }
+            }
+
+            return __COLUME__[base.key as keyof typeof __COLUME__]?.() ?? divineColumn(value)
+        }
+
+        return () => (
+            <u-container space="10px" style={{ margin: '0 10px 10px', backgroundColor: vars.value.cardColor }}>
+                <basic-table></basic-table>
+            </u-container>
+        )
     }
 })
 </script>
