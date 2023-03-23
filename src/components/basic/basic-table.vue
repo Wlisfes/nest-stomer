e13c7fec9f04fc2734df812a9625d85f070c8725
Wlisfes <876451336@qq.com>
2023-03-23 18:16:25 +0800

    ✨ feat(Basic): 修改基础组件

--- src/components/basic/basic-table.vue
+++ src/components/basic/basic-table.vue
@@ -4,22 +4,20 @@ import { defineComponent, Fragment } from 'vue'
 export default defineComponent({
     name: 'BasicTable',
     setup(props, { slots }) {
-        console.log(slots)
+        // console.log(slots)
         return () => {
             return (
                 <u-container class="basic-table">
-                    <div class="basic-table__toolbar">
-                        <div class="n-start">
-                            <Fragment>{slots.start?.()}</Fragment>
-                        </div>
-                        <div class="n-end">
-                            <n-space justify="end" size={[10, 8]}>
-                                {Array.from({ length: 10 }, (n, x) => (
-                                    <n-button type="primary">Oops! {x}</n-button>
-                                ))}
-                            </n-space>
-                        </div>
-                    </div>
+                    <basic-toolbar>
+                        {{
+                            start: () => (
+                                <Fragment>
+                                    <n-button type="primary">Oops!</n-button>
+                                    <n-button type="primary">Oops!</n-button>
+                                </Fragment>
+                            )
+                        }}
+                    </basic-toolbar>
                 </u-container>
             )
         }
--- src/components/basic/basic-toolbar.vue
+++ src/components/basic/basic-toolbar.vue
@@ -1,17 +1,23 @@
 <script lang="tsx">
 import { defineComponent, Fragment } from 'vue'
+import { useRxicon } from '@/hooks/hook-icon'
 
 export default defineComponent({
     name: 'BasicToolbar',
-    render() {
+    setup(props, { slots, emit }) {
+        const { Icon, compute } = useRxicon()
+
         return () => (
-            <div class="basic-toolbar">
-                <Fragment class="kkk">
-                    {Array.from({ length: 10 }, (n, x) => (
-                        <n-button type="primary">Oops! {x}</n-button>
-                    ))}
-                </Fragment>
-            </div>
+            <n-space size={[10, 8]} wrap-item={false}>
+                <Fragment>{slots.start?.()}</Fragment>
+
+                <n-tooltip trigger="hover">
+                    {{
+                        trigger: () => <Icon component={compute('ReloadOutlined')}></Icon>,
+                        default: () => <n-text tag="span">刷新</n-text>
+                    }}
+                </n-tooltip>
+            </n-space>
         )
     }
 })
