58bcf5bda4809711120833cd8f21028f7d4ee679
Wlisfes <876451336@qq.com>
2023-03-23 16:33:56 +0800

    ✨ feat(Basic): 修改基础组件

---
+++ src/components/basic/basic-table.vue
@@ -0,0 +1,34 @@
+<script lang="tsx">
+import { defineComponent, Fragment } from 'vue'
+
+export default defineComponent({
+    name: 'BasicTable',
+    setup(props, { slots }) {
+        console.log(slots)
+        return () => {
+            return (
+                <u-container class="basic-table">
+                    <div class="basic-table__toolbar">
+                        <div class="n-start">
+                            <Fragment>{slots.start?.()}</Fragment>
+                        </div>
+                        <div class="n-end">
+                            <n-space justify="end" size={[10, 8]}>
+                                {Array.from({ length: 10 }, (n, x) => (
+                                    <n-button type="primary">Oops! {x}</n-button>
+                                ))}
+                            </n-space>
+                        </div>
+                    </div>
+                </u-container>
+            )
+        }
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.basic-table {
+    position: relative;
+}
+</style>
---
+++ src/components/basic/basic-toolbar.vue
@@ -0,0 +1,25 @@
+<script lang="tsx">
+import { defineComponent, Fragment } from 'vue'
+
+export default defineComponent({
+    name: 'BasicToolbar',
+    render() {
+        return () => (
+            <div class="basic-toolbar">
+                <Fragment class="kkk">
+                    {Array.from({ length: 10 }, (n, x) => (
+                        <n-button type="primary">Oops! {x}</n-button>
+                    ))}
+                </Fragment>
+            </div>
+        )
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.basic-toolbar {
+    position: relative;
+    overflow: hidden;
+}
+</style>
