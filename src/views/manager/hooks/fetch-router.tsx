ee1ab7a896cc26c95e8d846b1bf31c727621fa45
妖雨纯 <876451336@qq.com>
2023-04-02 10:41:12 +0800

    ✨ feat(Base): 新增弹窗拖拽工具

--- src/views/manager/hooks/fetch-router.tsx
+++ src/views/manager/hooks/fetch-router.tsx
@@ -1,9 +1,9 @@
 import { onMounted, ref } from 'vue'
-
 import { FormInst, FormRules } from 'naive-ui'
 import { useState } from '@/hooks/hook-state'
 import { createComponent } from '@/utils/utils-app'
 import { Observer } from '@/utils/utils-observer'
+import { transfer } from '@/utils/utils-transfer'
 
 export function fetchRouter() {
     const observer = new Observer<Record<string, unknown>>()
@@ -14,7 +14,9 @@ export function fetchRouter() {
             const { form, rules, state, setState } = useState({
                 visible: false,
                 loading: false,
-                form: {},
+                form: {
+                    title: undefined
+                },
                 rules: {}
             })
             // const rules: FormRules = {
@@ -22,22 +24,58 @@ export function fetchRouter() {
             //     name: [{ required: true, message: '请上传资源文件', trigger: 'change' }]
             // }
 
+            const onAfterEnter = (el: HTMLElement) => {
+                const screenWidth = document.body.clientWidth //body当前宽度
+                const screenHeight = document.documentElement.clientHeight //可见区域高度
+
+                const elWidth = el.offsetWidth //对话框宽度
+                const elHeight = el.offsetHeight //对话框高度
+                const minDomLeft = el.offsetLeft
+                const minDomTop = el.offsetTop
+
+                const { width, height, left, top } = el.getBoundingClientRect()
+                const element = el.querySelector('.n-dialog__title') as HTMLElement
+
+                element.onmousedown = e => {
+                    console.log({
+                        clientX: e.clientX,
+                        clientY: e.clientY
+                    })
+                }
+
+                document.onmouseup = e => {
+                    console.log(e)
+                }
+            }
+
             onMounted(() => setState({ visible: true }))
 
             return () => (
-                <n-modal v-model:show={state.visible} to={el}>
-                    <n-spin show={state.loading}>
-                        <n-form
-                            ref={formRef}
-                            model={state}
-                            rules={rules}
-                            disabled={state.loading}
-                            label-placement="left"
-                            require-mark-placement="left"
-                            label-width={85}
-                            style={{ margin: '24px 0' }}
-                        ></n-form>
-                    </n-spin>
+                <n-modal
+                    v-model:show={state.visible}
+                    closable={true}
+                    auto-focus={false}
+                    to={el}
+                    transform-origin="mouse"
+                    title="重置密码"
+                    preset="dialog"
+                    onAfterLeave={unmount}
+                    onAfterEnter={transfer}
+                >
+                    <n-form
+                        ref={formRef}
+                        model={state}
+                        rules={rules}
+                        disabled={state.loading}
+                        label-placement="left"
+                        require-mark-placement="left"
+                        label-width={85}
+                        style={{ margin: '24px 0' }}
+                    >
+                        <n-form-item label="密码" path="password">
+                            <n-input v-model:value={form.value.title} size="medium" placeholder="密码"></n-input>
+                        </n-form-item>
+                    </n-form>
                 </n-modal>
             )
         }
