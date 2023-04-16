aa56d55e73986936600d6c10e0aaf8e1f93cdeb7
妖雨纯 <876451336@qq.com>
2023-04-16 22:54:06 +0800

    ✨ feat(cloud): 新增视频点播类别

---
+++ src/views/manager/cloud/Similar.vue
@@ -0,0 +1,15 @@
+<script lang="tsx">
+import { defineComponent } from 'vue'
+import { router } from '@/router'
+
+export default defineComponent({
+    name: 'Similar',
+    setup() {
+        return () => (
+            <u-container space="16px" style={{ margin: '0 10px 10px' }}>
+                Similar
+            </u-container>
+        )
+    }
+})
+</script>
