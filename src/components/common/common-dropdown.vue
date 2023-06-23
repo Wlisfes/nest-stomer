b095b11aaf200f0a2c991f288bb9af96411dcfd6
妖雨纯 <876451336@qq.com>
2023-06-23 21:25:45 +0800

    ✨ feat(common): 新增下拉菜单组件

---
+++ src/components/common/common-dropdown.vue
@@ -0,0 +1,21 @@
+<script lang="tsx">
+import { defineComponent, computed, type PropType } from 'vue'
+import { type DropdownOption } from 'naive-ui'
+
+export default defineComponent({
+    name: 'CommonDropdown',
+    props: {
+        showArrow: { type: Boolean, default: true },
+        trigger: { type: String as PropType<'hover' | 'click' | 'focus' | 'manual'>, default: 'click' },
+        command: { type: Array as PropType<Array<unknown>>, default: () => [] }
+    },
+    setup(props) {
+        const dataCommand = []
+        return (
+            <n-dropdown trigger={props.trigger} placement="top" show-arrow={props.showArrow}>
+                <n-button>找个地方休息</n-button>
+            </n-dropdown>
+        )
+    }
+})
+</script>
