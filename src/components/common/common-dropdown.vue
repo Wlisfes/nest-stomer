4a84c221b8152883f856e1de78ed70df2ee667e4
妖雨纯 <876451336@qq.com>
2023-07-09 11:56:21 +0800

    ✨ feat(common): 新增下拉菜单指令

--- src/components/common/common-dropdown.vue
+++ src/components/common/common-dropdown.vue
@@ -2,13 +2,14 @@
 import { defineComponent, computed, Fragment, createVNode, type PropType } from 'vue'
 import { type DropdownOption } from 'naive-ui'
 import { useCurrent } from '@/locale/instance'
+import { sompute } from '@/utils/utils-remix'
 
 export default defineComponent({
     name: 'CommonDropdown',
     props: {
         showArrow: { type: Boolean, default: true },
         trigger: { type: String as PropType<'hover' | 'click' | 'focus' | 'manual'>, default: 'click' },
-        command: { type: Array as PropType<Array<'create' | 'update' | 'delete'>>, default: () => [] },
+        command: { type: Array as PropType<Array<'create' | 'update' | 'delete' | 'disable' | 'enable'>>, default: () => [] },
         placement: { type: String, default: 'top' }
     },
     emits: ['selecter'],
@@ -17,7 +18,9 @@ export default defineComponent({
         const dataSource = computed<Array<DropdownOption>>(() => [
             { label: t('common.create.value'), key: 'create' },
             { label: t('common.update.value'), key: 'update' },
-            { label: t('common.delete.value'), key: 'delete' }
+            { label: t('common.delete.value'), key: 'delete' },
+            { label: t('common.disable.value'), key: 'disable' },
+            { label: t('common.enable.value'), key: 'enable' }
         ])
         const dataColumn = computed(() => dataSource.value.filter(x => props.command.includes(x.key as never)))
 
@@ -25,17 +28,15 @@ export default defineComponent({
         function createRemix(option: DropdownOption) {
             switch (option.key) {
                 case 'create':
-                    return createVNode(
-                        <common-remix type="primary" hover={false} icon={<n-icon component={<Icon-AddBold />}></n-icon>}></common-remix>
-                    )
+                    return createVNode(<common-remix type="primary" hover={false} icon={sompute('AddBold')}></common-remix>)
                 case 'update':
-                    return createVNode(
-                        <common-remix type="info" hover={false} icon={<n-icon component={<Icon-RadixEdit />}></n-icon>}></common-remix>
-                    )
+                    return createVNode(<common-remix type="info" hover={false} icon={sompute('RadixEdit')}></common-remix>)
                 case 'delete':
-                    return createVNode(
-                        <common-remix type="error" hover={false} icon={<n-icon component={<Icon-DeleteBold />}></n-icon>}></common-remix>
-                    )
+                    return createVNode(<common-remix type="error" hover={false} icon={sompute('DeleteBold')}></common-remix>)
+                case 'disable':
+                    return createVNode(<common-remix type="warning" hover={false} icon={sompute('DisableBold')}></common-remix>)
+                case 'enable':
+                    return createVNode(<common-remix type="success" hover={false} icon={sompute('EnableBold')}></common-remix>)
             }
         }
 
